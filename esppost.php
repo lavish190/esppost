4b32d44dee8609be7055d51aeee60fd03b56844e
LAVISH SHARMA <56466819+lavish190@users.noreply.github.com>
2024-10-27 12:40:52 +0530

    Create esppost.php

---
+++ esppost.php
@@ -0,0 +1,10 @@
+< ?php
+$Temp=$ POST ["temperature");
+// create a variable that will receive the temperature
+$Humidity=$ POST "humidity") ;
+// create a variable that will receive the humidity
+$Temp
+" Celcius
+// create an HTML file to display the data within
+file put contents ( sensor.html , $Write) ;
+$Humidity .
